ab3a394957614652046c2948b1b2acd87171567b
leihan <han.lei@mail.hypers.com>
2020-01-18 18:31:55 +0800

    update ani style

--- client/src/components/InfoCard.tsx
+++ client/src/components/InfoCard.tsx
@@ -4,12 +4,11 @@ import Card from '@/components/Card'
 import _get from 'lodash.get'
 import _isEmpty from 'lodash.isempty'
 import classNames from 'classnames'
-import { getRect, getRectOffset } from '@/utils/dom'
 import { formatDateOrDuring, parseFirstSentence } from '@/utils'
 import request from '@/utils/request'
-import {API_READ_HUB_TOPIC} from '@/constants/api'
-import './InfoCard.scss'
+import { API_READ_HUB_TOPIC } from '@/constants/api'
 import { stylePrefix } from '@/utils/prefix'
+import './InfoCard.scss'
 
 const addStylePrefix = stylePrefix('info-card')
 
@@ -19,7 +18,6 @@ interface InfoCardProps {
 
 interface InfoCardState {
   active: boolean
-  containerStyle: BaseObject
   detailData: BaseObject
 }
 
@@ -31,36 +29,22 @@ export default class InfoCard extends Taro.Component<
     data: {}
   }
 
-  containerRect: BaseObject<number>
-  containerRectOffset: BaseObject<number>
-  animationData: BaseObject[] = []
-
   constructor(props) {
     super(props)
     this.state = {
       active: false,
-      containerStyle: {},
       detailData: {}
     }
   }
 
-  componentDidMount(): void {
-    getRect(
-      '.info-card-container',
-      rect => {
-        this.containerRect = rect
-      },
-      this.$scope
-    )
-    getRectOffset(rect => (this.containerRectOffset = rect), this.$scope)
-  }
+  componentDidMount(): void {}
 
   updateDetail = () => {
     const {
       data: { id }
     } = this.props
     return (
-      _isEmpty( this.state.detailData) &&
+      _isEmpty(this.state.detailData) &&
       request({
         url: API_READ_HUB_TOPIC({ id }),
         parse: res => _get(res, 'data', {})
@@ -71,53 +55,20 @@ export default class InfoCard extends Taro.Component<
     )
   }
 
-  getPopupAni = () =>
-    Taro.createAnimation({
-      transformOrigin: '50% 50%',
-      timingFunction: 'ease',
-      delay: 0
-    })
-      .scale(0.9, 0.9)
-      .step({ duration: 50 })
-      .scale(1, 1)
-      .step({ duration: 50, delay: 50 })
-      .export()
-
-  getPopDownAni = () =>
-    Taro.createAnimation({
-      transformOrigin: '50% 50%',
-      timingFunction: 'ease',
-      delay: 0
-    })
-      .step({ duration: 25 })
-      .step({ duration: 25, delay: 25 })
-      .export()
-
-  handleLongPress = () => {
-    this.animationData = this.getPopupAni()
-    Taro.vibrateShort().then(() => {
-      setTimeout(() => {
-        this.setState({
-          containerStyle: {
-            width: `${this.containerRect.width}px`,
-            height: `${this.containerRect.height}px`,
-            marginBottom: '30rpx'
-          },
-          active: true
-        })
-        // 抓取详细数据
-        this.updateDetail()
-      }, 100)
+  fetch = () => {
+    this.setState({
+      active: true
     })
   }
 
-  handleClosePreview = () => {
-    this.animationData = this.getPopDownAni()
-    this.setState({ active: false })
-  }
-
-  handleAnimationEnd = () => {
-    this.animationData = []
+  handleClick = () => {
+    const { active } = this.state
+    const nextActive = !active
+    if (nextActive) {
+      // 抓取详细数据
+      this.updateDetail()
+    }
+    this.setState({ active: nextActive })
   }
 
   render(): any {
@@ -126,30 +77,21 @@ export default class InfoCard extends Taro.Component<
     } = this.props
     const {
       active,
-      containerStyle,
       detailData: { summary: detailSummary, createdAt: detailCreatedAt }
     } = this.state
-    const cls = classNames(addStylePrefix('container'), {
-      [addStylePrefix('container-active')]: active
-    })
-    const desc = active ? detailSummary : parseFirstSentence(summary)
     const time = formatDateOrDuring(active ? detailCreatedAt : createdAt)
     return (
-      <View className={cls} style={containerStyle}>
-        <Card
-          onLongPress={this.handleLongPress}
-          onClick={this.handleClosePreview}
-          className={classNames({
-            [addStylePrefix('container-active-main')]: active
+      <Card onClick={this.handleClick} className={classNames('info-card')}>
+        <View className={addStylePrefix('title')}>{title}</View>
+        <View
+          className={classNames(addStylePrefix('description'), {
+            [addStylePrefix('description-active')]: detailSummary && active
           })}
-          animation={this.animationData}
-          onAnimationEnd={this.handleAnimationEnd}
         >
-          <View className={addStylePrefix('title')}>{title}</View>
-          <View className={addStylePrefix('description')}>{desc}</View>
-          <View className={addStylePrefix('time')}>{time}</View>
-        </Card>
-      </View>
+          {detailSummary}
+        </View>
+        <View className={addStylePrefix('time')}>{time}</View>
+      </Card>
     )
   }
 }
